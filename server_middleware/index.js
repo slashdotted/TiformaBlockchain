c91d5ce5e610f2d530fc8606f5ced1eb02c13447
ItsMeZy <38465576+ItsMeZy@users.noreply.github.com>
2019-07-04 18:35:08 +0200

    add middleware files

---
+++ server_middleware/index.js
@@ -0,0 +1,34 @@
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+
+var businessNetwork = new BusinessNetworkConnection();
+
+return businessNetwork.connect('admin@supsi-tiforma')
+.then(function(businessNetworkDefinition){
+    console.log('Connected');
+    return businessNetwork.getAssetRegistry('ch.supsi.CreateStudent')
+  .then(function (studentAssetRegistry) {
+    console.log('Entered');
+    return studentAssetRegistry.getAll();
+  })
+  .then(function (students) {
+    console.log('Entered');
+    students.forEach(function (student) {
+      console.log(student.name);
+    });
+  })
+  .catch(function (error) {
+    
+  });
+});
+
+/*
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+this.bizNetworkConnection = new BusinessNetworkConnection();
+let businessNetworkDefinition = await this.bizNetworkConnection.connect('admin@supsi-tiforma');
+this.titlesRegistry = await this.bizNetworkConnection.getAssetRegistry('ch.supsi.Student');
+let aResources = await registry.getAll();
+
+let arrayLength = aResources.length;
+for (let i = 0; i < arrayLength; i++) {
+    console.log(aResources[i].name);
+}*/
