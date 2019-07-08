0d98921aa9242a8ae89345fec14f22cc6d6c7b4a
zyrilerrol <zyrilerrol.gatchalian@student.supsi.ch>
2019-07-08 17:56:12 +0200

    student problem resolved

--- server_middleware/index.js
+++ server_middleware/index.js
@@ -1,60 +1,55 @@
 const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
 
-var businessNetwork = new BusinessNetworkConnection();
+
 /*GET ALL STUDENTS */
-// return businessNetwork.connect('admin@supsi-tiforma')
-// .then(function(businessNetworkDefinition){
-//     console.log('Connected');
-//     return businessNetwork.getParticipantRegistry('ch.supsi.Student')
-//   .then(function (studentAssetRegistry) {
-//     console.log('Entered');
-//     return studentAssetRegistry.getAll();
-//   })
-//   .then(function (students) {
-//     console.log('Entered');
-//     students.forEach(function (student) {
-//       console.log(student.name);
-//     });
-//   })
-//   .catch(function (error) {
-    
-//   });
-// });
+async function getStudents(){
+    try{
+        let businessNetworkConnection = new BusinessNetworkConnection();
+        const businessNetworkDefinition=  await businessNetworkConnection.connect('admin@supsi-tiforma');
+        let studentRegistry = await businessNetworkConnection.getParticipantRegistry('ch.supsi.Student');
+        let students= await studentRegistry.getAll();
+        students.forEach(student => {
+            console.log(student.contactID);
+        });
+        await businessNetworkConnection.disconnect();
+    }catch(error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
 
 /* GET STUDENT{ID}*/
-// return businessNetwork.connect('admin@supsi-tiforma')
-// .then(function(businessNetworkDefinition){
-//     console.log('Connected');
-//     return businessNetwork.getParticipantRegistry('ch.supsi.Student')
-//   .then(function (studentAssetRegistry) {
-//     console.log('Entered 1');
-//     return studentAssetRegistry.get('39uozg4tbhdz7hj4sktbo');
-//   })
-//   .then(function (student) {
-//       console.log('Entered 2');
-//       console.log(student.name);
-//   })
-//   .catch(function (error) {
-//     console.log('Catched Error');
-//   });
-// });
+async function getStudentsById() {
+    try{
+        let businessNetworkConnection = new BusinessNetworkConnection();
+        const businessNetworkDefinition=  await businessNetworkConnection.connect('admin@supsi-tiforma');
+        let studentRegistry = await businessNetworkConnection.getParticipantRegistry('ch.supsi.Student');
+        let student= await studentRegistry.get('obcfqho9k9nfnjvwkskdg');
+        console.log(student.name);
+        await businessNetworkConnection.disconnect();
+    }catch(error) {
+        console.error(error);
+        process.exit(1);
+    }
+  
+}
 /*CREATE A NEW STUDENT */
 
 
-async function addParticipant() {
+async function addStudent() {
     let businessNetworkConnection = new BusinessNetworkConnection();
 
     try {
-        await businessNetworkConnection.connect('admin@supsi-tiforma');
+        const definition=await businessNetworkConnection.connect('admin@supsi-tiforma');
         let participantRegistry = await businessNetworkConnection.getParticipantRegistry('ch.supsi.Student');
-        let factory = businessNetworkConnection.getFactory();
-        let participant = factory.newResource('ch.supsi', 'Student', '12345ABC');
+        var factory = definition.getFactory();
+        var participant = factory.newResource('ch.supsi', 'Student', '12345ABCZS');
         participant.name = 'Mae';
         participant.surname = 'Smith';
-        participant.birthday="2019-05-16T00:00:00.0Z";
+        participant.birthday=new Date("2019-05-16T00:00:00.0Z");
         participant.nationality="swiss";
         participant.statute="Immatricolato";
-        participant.serialNumber= "123455"
+        participant.serialNumber= "12345567"
         await participantRegistry.add(participant);
         await businessNetworkConnection.disconnect();
     } catch(error) {
@@ -63,4 +58,6 @@ async function addParticipant() {
     }
 }
 
-addParticipant();
+//addStudent();
+//addStudent();
+//getStudents();
